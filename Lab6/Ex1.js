1edd04b5a16596bed3e12b942752621a75537acd
knsuna <knsunaha@hawaii.edu>
2020-09-22 11:51:29 -1000

    ex lab 6

---
+++ Lab6/Ex1.js
@@ -0,0 +1,8 @@
+var age = 19;
+var fav_num = 21;
+var day_of_birth = 4;
+var month_of_birth = 12;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
