ab9acd93db899a5561f8a0aba3f6e59f746e280f
knsuna <k.suna246@gmail.com>
2020-11-12 19:29:20 -1000

    update

--- Sunahara_Kai_Assignment2/server.js
+++ Sunahara_Kai_Assignment2/server.js
@@ -30,7 +30,7 @@ app.post("/process_form", function (request, response) {
         `)
     }
     if (isNonNegInt(productquantitites)) {
-        display_invoice_table_rows(POST,response);
+        response.redirect(`./login`);
     }     
     else {
         response.send(`
@@ -47,7 +47,7 @@ app.post("/process_form", function (request, response) {
     var contents = fs.readFileSync('./public/invoice.view', 'utf8');
     response.send(eval('`' + contents + '`')); // render template string
     
-    //Taken from Assignment 1 example. 
+        //Taken from Assignment 1 example. 
     function display_invoice_table_rows() {
         subtotal = 0;
         str = '';
@@ -91,8 +91,78 @@ app.post("/process_form", function (request, response) {
         return str;
     }
 
-});;
+});
 
+const user_data_filename = `user_data.json`;
+
+//Taken from Lab14. Checks if the login already exists
+if (fs.existsSync(user_data_filename)) {
+    data = fs.readFileSync(user_data_filename, 'utf-8');
+
+    stats = fs.statSync(user_data_filename)
+    console.log(user_data_filename + ' has ' + stats.size + ' characters');
+
+    var user_reg_data = JSON.parse(data); // Takes a string and converts it into object or array
+
+    fs.writeFileSync(user_data_filename, JSON.stringify(user_reg_data));
+
+    console.log(user_reg_data);
+} else {
+    console.log(user_data_filename + ' does not exist!');
+}
+
+app.get("/login", function (request, response) {
+    var login = fs.readFileSync(`./public/login.html`,`utf-8`);
+    response.redirect(login);
+});
+;
+
+app.post("/process_login", function (request, response) {
+    // Process login form POST and redirect to logged in page if ok, back to login page if not
+    console.log(request.body);
+    // Checks if username exists already USE FOR LOGIN CHECK
+    if (typeof user_reg_data[request.body.username] != 'undefined') {
+        if (user_reg_data[request.body.username].password == request.body.password) {
+            display_invoice_table_rows(request.body.username,response);
+        } else {
+            response.redirect('./login'); //REDIRECT to Login HTML
+        }
+    }
+});
+
+
+// CHANGE to Register HTML
+app.get("/register", function (request, response) {
+    // Give a simple register form
+    str = `
+        <body>
+        <form action="" method="POST">
+        <input type="text" name="username" size="40" placeholder="enter username" ><br />
+        <input type="password" name="password" size="40" placeholder="enter password"><br />
+        <input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+        <input type="email" name="email" size="40" placeholder="enter email"><br />
+        <input type="submit" value="Submit" id="submit">
+        </form>
+        </body>
+    `;
+    response.send(str);
+});
+
+app.post("/register", function (request, response) {
+    // process a simple register form
+
+    //validate registration data
+
+    //all good so save the new user
+    username = request.body.username;
+    user_reg_data[username] = {};
+    user_reg_data[username].password = request.body.password;
+    user_reg_data[username].email = request.body.email;
+
+    fs.writeFileSync(user_data_filename, JSON.stringify(user_reg_data));
+    
+    response.send(`${username} registered!`);
+});
 
 function isNonNegInt(q, return_errors = false) {
     errors = []; // assume no errors at first
